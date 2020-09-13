e7465ec443d0dd6d968444fc807db9acc6ecc565
KyleDolezal <kyle.d.dolezal@gmail.com>
2020-09-13 15:59:33 -0500

    [Fix] Update spring security to allow static assets to be accessed

--- src/main/kotlin/io/github/kyledolezal/ToutTracker/security/SecurityConfig.kt
+++ src/main/kotlin/io/github/kyledolezal/ToutTracker/security/SecurityConfig.kt
@@ -5,27 +5,17 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 class SecurityConfig : WebSecurityConfigurerAdapter() {
-    override fun configure(http: HttpSecurity): Unit {
-        http
-            .authorizeRequests()
-            .antMatchers("/").permitAll()
-            .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-            .antMatchers("/index.html").permitAll()
-            .antMatchers("/admin").hasRole("ADMIN")
-            .anyRequest().authenticated()
-            .and()
-            .formLogin()
-            .loginPage("/login")
-            .permitAll()
-            .and()
-            .logout()
-            .permitAll()
-
-        http.exceptionHandling().accessDeniedPage("/403");
+    override fun configure(web: WebSecurity): Unit {
+        web
+            .ignoring()
+            .antMatchers("/")
+            .antMatchers("/static/**")
+            .antMatchers("/index.html")
     }
 
     @Autowired
