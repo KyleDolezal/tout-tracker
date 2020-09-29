ee88872aafd4b46336c9a507a4bed94cdacc7fa3
KyleDolezal <kyle.d.dolezal@gmail.com>
2020-09-28 21:27:24 -0500

    [New] Redirect to client frontend app from root URL

--- src/main/kotlin/io/github/kyledolezal/touttracker/controllers/StaticController.kt
+++ src/main/kotlin/io/github/kyledolezal/touttracker/controllers/StaticController.kt
@@ -1,14 +1,13 @@
 package io.github.kyledolezal.touttracker.controllers
 
-import org.springframework.http.ResponseEntity
-import org.springframework.http.ResponseEntity.*
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.HttpServletResponse
 
 @RestController
 class StaticController {
     @GetMapping("/")
-    fun root() : ResponseEntity<Any>{
-        return ok().body("success")
+    fun root(response: HttpServletResponse) {
+        response.sendRedirect("index.html")
     }
 }
